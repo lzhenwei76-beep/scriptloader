013d1d8686f5017b883f900a45b9f460265da6e0
ZW <lzhenwei76@gmail.com>
2026-03-25 11:35:57 +0100

    Create bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,26 @@
+(async function() {
+    'use strict';
+    
+    const CSS_URL = 'https://raw.githubusercontent.com/lzhenwei76-beep/MathlethicsCheat/main/loader.css';
+    const LOADER_URL = 'https://raw.githubusercontent.com/lzhenwei76-beep/MathlethicsCheat/main/loader.js';
+    const HACK_URL = 'https://raw.githubusercontent.com/lzhenwei76-beep/MathlethicsCheat/main/hack.js';
+    
+    // CSS laden
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = CSS_URL;
+    document.head.appendChild(link);
+    
+    // Loader laden
+    const response = await fetch(LOADER_URL);
+    const scriptCode = await response.text();
+    eval(scriptCode);
+    
+    // Loader starten mit Hack-URL
+    if (window.MathLoader) {
+        window.MathLoader.init(HACK_URL);
+    } else {
+        console.error('Loader not loaded correctly');
+    }
+    
+})();
